b835006db0d84a2d0b95dbd70b132c27948bf292
Kyle Neale <kyle.a.neale@gmail.com>
2018-06-29 17:31:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(string)
   elsif string[string.length - 1] == '!'||string == string.upcase
     "NO, NOT SINCE 1938!"
   else
-    "HUH?!, SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
   
